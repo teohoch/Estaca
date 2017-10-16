5606887c476588bed89b48253db30a83bafc2d14
Teodoro Hochfärber <Teohoch2@gmail.com>
2017-10-16 00:32:24 -0300

    pregunta 2.2

--- Lec2/pregunta2-2.R
+++ Lec2/pregunta2-2.R
@@ -1,6 +1,6 @@
 repeats <- 10000
 
-DardosDistancia <-runif(repeats,1,11)
+DardosDistancia <-runif(repeats,5,11)
 hitmiss <- c(0,1,1,1,1,1,1,1,1,1)
 Miss <- replicate(repeats,sample(hitmiss,1,replace = TRUE))
 probs <- c(0,0,0,0,0,0,0,0,0,0)
@@ -13,7 +13,7 @@ p6 <- c()
 
 for(i in 1:repeats){
   anillo <- floor(DardosDistancia[i])
-  probs[anillo] <- probs[anillo]+1 
+  probs[anillo] <- probs[anillo]+Miss[i] 
   
     p10 <- c(p10,probs[10]/i)
     p9 <- c(p9,probs[9]/i)
@@ -22,39 +22,42 @@ for(i in 1:repeats){
     p6 <- c(p6,probs[6]/i)
 }
 
+expected <- 0.15
+
 for(i in 1:10){
   probs2[i] <- probs[i]/repeats
-  print( sprintf("Anillo %s - Probabilidad %s Diff %s", i, probs2[i], abs(probs2[i]-(1/10)) ))
+  print( sprintf("Anillo %s - Probabilidad %s Diff %s", i, probs2[i], abs(probs2[i]-(expected)) ))
 }
+
+
 
 plot(1:repeats,p10,
      xlab = "Numero de repeticiones",
      ylab = "Probabilidad",
      main = "Ganar un almuerzo gratis")
-abline(h=1/10,col='red',lwd=2)
+abline(h=expected,col='red',lwd=2)
 
 plot(1:repeats,p9,
      xlab = "Numero de repeticiones",
      ylab = "Probabilidad",
      main = "Ganar bebida extra")
-abline(h=1/10,col='red',lwd=2)
+abline(h=expected,col='red',lwd=2)
 
 plot(1:repeats,p8,
      xlab = "Numero de repeticiones",
      ylab = "Probabilidad",
      main = "Ganar un postre")
-abline(h=1/10,col='red',lwd=2)
+abline(h=expected,col='red',lwd=2)
 
 plot(1:repeats,p7,
      xlab = "Numero de repeticiones",
      ylab = "Probabilidad",
      main = "Ganar descuento de $500")
-abline(h=1/10,col='red',lwd=2)
+abline(h=expected,col='red',lwd=2)
 
 plot(1:repeats,p6,
      xlab = "Numero de repeticiones",
      ylab = "Probabilidad",
      main = "Ganar otro tiro")
-abline(h=1/10,col='red',lwd=2)
-
+abline(h=expected,col='red',lwd=2)
 
